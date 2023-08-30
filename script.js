f65239f33d9837f012192a842c841bf52d21bc70
Youssef Fatih <yousseffatih@Youssefs-MacBook-Pro.local>
2023-08-30 14:12:10 +0100

    Complet Js File and make some modification in other files

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function addTaskCard(task ,index){
                   <span class="task-id">#${task.taskId}</span>
                   <span>
                         <button class="bi bi-pencil edit" data-id="${task.taskId}"></button>
-                        <button class="bi bi-check-lg update" data-id="${task.taskId}"></button>
+                        <button class="bi bi-check-lg update hide" data-id="${task.taskId}" data-column="${index}"></button>
                         <button class="bi bi-trash3 delete" data-id="${task.taskId}"></button>
                   </span>
             </div>
@@ -44,6 +44,64 @@ addForm.forEach(form =>{
             
       });
 })
+
+taskbox.forEach(column =>{
+      column.addEventListener("click", (event)=>{
+            event.preventDefault();
+            const formInput = event.target.parentElement.parentElement.previousElementSibling;
+            if(event.target.classList.contains("edit"))
+            {
+                  formInput.removeAttribute("disabled");
+                  event.target.classList.add("hide");
+                  event.target.nextElementSibling.classList.remove("hide");
+            }
+            if(event.target.classList.contains("update"))
+            {
+                  formInput.setAttribute("disabled","disabled     ");
+                  event.target.classList.add("hide");
+                  event.target.previousElementSibling.classList.remove("hide");
+                  const taskId = event.target.dataset.id;
+                  const columnId = event.target.dataset.column;
+                  const content = formInput.value;
+                  console.log(taskId , columnId ,content);
+                  kanban.updateTask(taskId , {
+                        columnId : columnId,
+                        content : content
+                  });
+            }
+
+            if(event.target.classList.contains("delete")){
+                  event.preventDefault();
+                  formInput.parentElement.remove();
+                  kanban.deleteTask(event.target.dataset.id);
+            }
+      });
+      column.addEventListener("dragstart" ,event => {
+            if(event.target.classList.contains("card"))
+            {
+                  event.target.classList.add("dragging");
+            }
+      });
+
+      column.addEventListener("dragover",event =>{
+            const card = document.querySelector(".dragging");
+            column.appendChild(card);
+      });
+
+      column.addEventListener("dragend",event =>{
+            if(event.target.classList.contains("card")){
+                  event.target.classList.remove("dragging");
+                  const taskId = event.target.dataset.id;
+                  const columnId = event.target.parentElement.dataset.id;
+                  const content = event.target.task.value;
+                  kanban.updateTask(taskId ,{
+                        columnId:columnId,
+                        content:content
+                  });
+            }
+      });
+})
+
 
 //console.log(kanban.getAllTask());
 //console.log(kanban.insertTask(1,"Edit Kanban"));
